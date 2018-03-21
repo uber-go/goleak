3bf9c87ef2207ecdd78dfdb11afeda82877b144a
Martynas Buivys <martynas.buivys@gmail.com>
2018-03-21 03:10:15 +0200

    Indent a code example in a comment (#17)

--- testmain.go
+++ testmain.go
@@ -41,9 +41,9 @@ type TestingM interface {
 // verify that there were no goroutine leaks.
 // To use it, your TestMain function should look like:
 //
-// func TestMain(m *testing.M) {
-//   goleak.VerifyTestMain(m)
-// }
+//  func TestMain(m *testing.M) {
+//    goleak.VerifyTestMain(m)
+//  }
 //
 // This will run all tests as per normal, and if they were successful, look
 // for any goroutine leaks and fail the tests if any leaks were found.
