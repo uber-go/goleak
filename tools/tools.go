f8d5331e47115712d3462f818f736f6353fc42f4
Prashant V <prashant@prashantv.com>
2023-02-05 22:46:28 -0800

    Move tools dependencies into subdirectory with tag
    Avoid tools dependencies in goleak's go.mod.
    
    golint is deprecated, we can separately migrate to another tool, but
    the tools change avoids any CI-time dependency from leaking into our
    go.mod.

--- tools/tools.go
+++ tools/tools.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Uber Technologies, Inc.
+// Copyright (c) 2023 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,7 @@
 //go:build tools
 // +build tools
 
-package goleak
+package tools
 
 import (
 	// Tools we use during development.
